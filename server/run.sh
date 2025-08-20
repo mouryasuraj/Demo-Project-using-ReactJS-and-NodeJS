97e9aefd44531af2be719b197bed891c651ba4c5
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-08-20 16:30:56 +0530

    added env file during running container

--- server/run.sh
+++ server/run.sh
@@ -2,5 +2,5 @@ docker stop backend;
 docker rm backend;
 docker rmi backend;
 docker build -t backend .;
-docker run -d --name backend -p 5001:5001 backend;
+docker run -d --name backend -p 5001:5001 --env-file .env backend;
 docker logs -f backend;
