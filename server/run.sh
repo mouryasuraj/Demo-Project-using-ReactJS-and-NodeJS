051a297e1facccfe48588e5c02e10ad78aad6ff9
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-08-20 16:27:35 +0530

    added log command in sh file

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,6 @@
 docker stop backend;
 docker rm backend;
 docker rmi backend;
-docker build -t backend .
-docker run -d --name backend -p 5001:5001 backend
+docker build -t backend .;
+docker run -d --name backend -p 5001:5001 backend;
+docker logs -f backend;
