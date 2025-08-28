a25833503c32a41c6bf6907eefc0e947f41a344f
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-08-28 20:54:14 +0530

    Added webhook

--- server/controller/payment.controller.js
+++ server/controller/payment.controller.js
@@ -1,46 +1,88 @@
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils.js";
 import instance from "../connections/razorpay.js";
 import Payment from "../models/payment.model.js";
 import generateRandomNumber from "../utils/generateRandomNumber.js";
+import User from "../models/user.model.js";
 
-export const handleCreateOrder = async (req, res) =>{
+export const handleCreateOrder = async (req, res) => {
+  try {
+    const { type } = req.query;
+    const loggedInUser = req.user;
+    console.log(loggedInUser);
+
+    const amount = type === "silver" ? 500000 : type === "gold" ? 700000 : 0;
+
+    // Razor Pay - Create Order
+    const options = {
+      amount,
+      currency: "INR",
+      receipt: generateRandomNumber() + type + generateRandomNumber(),
+      partial_payment: false,
+      notes: {
+        fullName: loggedInUser.fullName || "",
+        memberShipType: type || "",
+      },
+    };
+    const order = await instance.orders.create(options);
+
+    const { id: orderId, currency, receipt, status, notes } = order;
+
+    // Save order detail in DB
+    const payment = new Payment({
+      userId: loggedInUser._id,
+      orderId,
+      currency,
+      amount,
+      receipt,
+      status,
+      notes,
+    });
+    const savedPayment = await payment.save();
+    res.json({
+      message: "Order created successfully",
+      order: {
+        ...savedPayment.toJSON(),
+        key_id: process.env.RAZORPAY_TEST_KEY_ID,
+      },
+    });
+  } catch (error) {
+    console.log("An error occured while createOrder: ", error);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+export const handleRazorWebhook = async (req, res) => {
     try {
-        const {type} = req.query;
-        const loggedInUser = req.user;
-        console.log(loggedInUser);
-        
-        const amount = type==="silver" ? 500000 : type==="gold" ? 700000 : 0
-
-        // Razor Pay - Create Order
-        const options = {
-            amount,
-            currency:"INR",
-            receipt:generateRandomNumber()+type+generateRandomNumber(),
-            partial_payment:false,
-            notes:{
-                fullName:loggedInUser.fullName || "",
-                memberShipType:type || ""
-            }
+        const reqBody = req.body
+        const signature = req.headers['X-Razorpay-Signature']
+        const isWebhookValid = validateWebhookSignature(reqBody, signature, process.env.RazorPaySurajSecretKey || "RazorPaySurajSecretKey")
+        console.log(isWebhookValid);
+        if(!isWebhookValid){
+            return res.status(400).json({message:"websignature is not valid"})
         }
-        const order = await instance.orders.create(options)
 
-        const {id:orderId,currency,receipt,status,notes} = order
+        const paymentDetails = reqBody.payload.payment.entity
+
+        // Change payment status 
+        const payment = await Payment.findOne({orderId:paymentDetails.order_id})
+        payment.status = paymentDetails.status
+
+        // Make user as premium member
+        if(reqBody.event==="payment.captured"){
+            const user = await User.findById({_id:payment.userId})
+            user.isPremium = true 
+            user.memberShipType = payment.memeberShipType
+        }
+        // if(reqBody.event==="payment.failed"){
+
+        // }
         
-
-        // Save order detail in DB
-        const payment = new Payment({
-            userId:loggedInUser._id,
-            orderId,
-            currency,
-            amount,
-            receipt,
-            status,
-            notes
-        })
-        const savedPayment = await payment.save()
-        res.json({message:"Order created successfully", order:{...savedPayment.toJSON(), key_id:process.env.RAZORPAY_TEST_KEY_ID}})
+        //Send response back to razorpay
+        res.json({message:"Webhook received successfully"})
         
     } catch (error) {
-        console.log("An error occured while createOrder: ",error)
+        console.log("An error occured in webhook: ",error)
         res.status(400).json({message:"Something went wrong"})
     }
-}
+};
+
--- server/routes/payment.router.js
+++ server/routes/payment.router.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { userAuth } from '../middlewares/userAuth.js';
-import { handleCreateOrder } from '../controller/payment.controller.js';
+import { handleCreateOrder, handleRazorWebhook } from '../controller/payment.controller.js';
 
 const paymentRouter = express.Router()
 
 
 paymentRouter.post("/createOrder", userAuth, handleCreateOrder)
+paymentRouter.post("/webhook", handleRazorWebhook)
 
 
 
